ed5305f186fd77d56f0151eee94573d766bc1622
Tim Tattersall <tim@tattsy.com>
2021-04-14 22:51:48 +1000

    Lemmatizing in the data processing stage 13% accuracy

--- keras/create_intents.py
+++ keras/create_intents.py
@@ -18,13 +18,13 @@ nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 
-words=[]
-classes = []
+words = set()
+classes = set()
 documents = []
 ignore_words = set(stopwords.words('english'))
 
 if __name__ == "__main__":
-    # Step 1 - Processing CSV file to produce JSON model
+    # Processing CSV file to produce JSON model
     with open('../SampleDataset-ParentAndChildrenWithTitles.csv', 'r') as dataset_file:
         dataset = list(csv.DictReader(dataset_file, skipinitialspace=True))
 
@@ -45,63 +45,63 @@ if __name__ == "__main__":
         })
 
         for title in row['ChildTitles']:
+            # Tokenize the word into its keywords
             title_tokens = set(nltk.word_tokenize(title))
+
+            # Remove stopwords from the set of tokens
             title_tokens.difference_update(ignore_words)
 
-            words.extend(title_tokens)
+            # Converting all words into their pure forms
+            lemmatized_tokens = set(map(lambda word: lemmatizer.lemmatize(word.lower()), title_tokens))
+
+            # Add keywords to set of words
+            words.update(lemmatized_tokens)
 
             # Linking words to a tag
-            documents.append((title_tokens, tag))
+            documents.append((lemmatized_tokens, tag))
 
-            # adding classes to our class list
-            if tag not in classes:
-                classes.append(tag)
+            # Adding the tag to the list of classes
+            classes.add(tag)
 
         counter += 1
-        # break
 
+    # Storing the intents for further examination45\
     with open('intents.json', 'w+') as intents_json:
         json.dump({
             'intents': intents,
-        }, intents_json)
+        }, intents_json, indent=2)
 
-    words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-    words = sorted(list(set(words)))
-
-    classes = sorted(list(set(classes)))
+    words = sorted(words)
+    classes = sorted(classes)
 
     print(len(documents), "documents")
-
     print(len(classes), "classes", classes)
-
     print(len(words), "unique lemmatized words", words)
 
     pickle.dump(words, open('words.pkl', 'wb'))
-    pickle.dump(classes,open('classes.pkl', 'wb'))
+    pickle.dump(classes, open('classes.pkl', 'wb'))
 
-
-    # initializing training data
+    # Producing Training Data
     training = []
     output_empty = [0] * len(classes)
-    for doc in documents:
+    for (pattern_words, class_name) in documents:
         # initializing bag of words
         bag = []
-        # list of tokenized words for the pattern
-        pattern_words = doc[0]
-        # lemmatize each word - create base word, in attempt to represent related words
-        pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+
         # create our bag of words array with 1, if word match found in current pattern
         for w in words:
-            bag.append(1) if w in pattern_words else bag.append(0)
+            bag.append(1 if w in pattern_words else 0)
 
         # output is a '0' for each tag and '1' for current tag (for each pattern)
         output_row = list(output_empty)
-        output_row[classes.index(doc[1])] = 1
+        output_row[classes.index(class_name)] = 1
 
         training.append([bag, output_row])
+
     # shuffle our features and turn into np.array
     random.shuffle(training)
     training = np.array(training)
+
     # create train and test lists. X - patterns, Y - intents
     train_x = list(training[:,0])
     train_y = list(training[:,1])
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-    #fitting and saving the model
+    # fitting and saving the model
     hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
     model.save('chatbot_model.h5', hist)
 
