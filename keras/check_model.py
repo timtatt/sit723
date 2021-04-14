283a050ef48b25606ca61dab619d082fdf9e6f57
Tim Tattersall <tim@tattsy.com>
2021-04-14 23:37:05 +1000

    Tidying check model methods

--- keras/check_model.py
+++ keras/check_model.py
@@ -31,30 +31,29 @@ def bag_of_words(sentence, words, show_details=True):
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
     p = bag_of_words(sentence, words, show_details=False)
-    res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    predictions = model.predict(np.array([p]))[0]
+
+    # filter out predictions below a threshold
+    error_threshold = 0.25
+    results = [[i, r] for i, r in enumerate(predictions) if r > error_threshold]
+
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
 
-def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if (i['tag'] == tag):
-            result = random.choice(i['responses'])
-            break
-    return result
+    return list(map(lambda x: {
+        "intent": classes[x[0]],
+        "probability": str(x[1])
+    }, results))
+
+def get_response(predictions, intents_json):
+    for intent in intents_json['intents']:
+        if intent['tag'] == predictions[0]['intent']:
+            return intent['responses'][0]
 
 def chatbot_response(msg):
     predictions = predict_class(msg, model)
-    return getResponse(predictions, intents) if len(predictions) > 0 else False
+    return get_response(predictions, intents) if len(predictions) > 0 else False
 
 if __name__ == "__main__":
     with open('../SampleDataset-ParentAndChildrenWithTitles.csv', 'r') as dataset_file:
